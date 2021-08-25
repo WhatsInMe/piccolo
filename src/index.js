c1c0bc84c75214f3684036a613e35b8623c4ed6b
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-25 01:20:49 -0700

    oauth variables

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const {
+  __CLIENT_ID__,
+  __CLIENT_SECRET__,
   __DB_HOST__,
-  __DB_USER__,
   __DB_NAME__,
   __DB_PASS__,
   __DB_PORT__,
-
+  __DB_USER__,
   __EXPRESS_PORT__,
 } = require("./utilities/constants");
 
@@ -14,13 +15,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const login = (req, res) => {};
+
+app.get("/login/github", login);
+
+/**
+ * debug
+ */
 app.get("/", (req, res) => {
   res.json({ mes: "sup" });
 });
-
-app.post("/", (req,res)=>{
-    res.send(req.body)
-})
+app.post("/", (req, res) => {
+  res.send(req.body);
+});
 
 app.listen(__EXPRESS_PORT__, () => {
   console.log(`running on port ${__EXPRESS_PORT__}`);
--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,12 +1,15 @@
+const __CLIENT_ID__ = process.env.GITHUB_CLIENT_ID;
+const __CLIENT_SECRET__ = process.env.GITHUB_CLIENT_SECRET;
 const __DB_HOST__ = process.env.POSTGRES_HOST;
 const __DB_NAME__ = process.env.POSTGRES_DB;
 const __DB_PASS__ = process.env.POSTGRES_PASS;
 const __DB_PORT__ = process.env.POSTGRES_PORT;
 const __DB_USER__ = process.env.POSTGRES_USER;
-
 const __EXPRESS_PORT__ = process.env.EXPRESS_PORT;
 
 module.exports = {
+  __CLIENT_ID__: __CLIENT_ID__,
+  __CLIENT_SECRET__: __CLIENT_SECRET__,
   __DB_HOST__: __DB_HOST__,
   __DB_NAME__: __DB_NAME__,
   __DB_PASS__: __DB_PASS__,
