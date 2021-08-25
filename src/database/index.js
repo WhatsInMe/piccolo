74cf94d42232154d1494dd0a1267b7c66bac5ace
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-25 04:20:41 -0700

    save

--- src/database/index.js
+++ src/database/index.js
@@ -6,8 +6,7 @@ const {
   __DB_USER__,
 } = require("../utilities/constants");
 
-const { Sequelize } = require("sequelize");
-
+const { Sequelize, Model, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(__DB_NAME__, __DB_USER__, __DB_PASS__, {
   host: __DB_HOST__,
   port: __DB_PORT__,
@@ -15,10 +14,19 @@ const sequelize = new Sequelize(__DB_NAME__, __DB_USER__, __DB_PASS__, {
   //   models: [Account, Session],
 });
 
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+const Account = sequelize.define("account", {
+  github_id: {
+    type: Sequelize.INTEGER,
+  },
+  access_token: {
+    type: Sequelize.STRING,
+  },
+});
 
-db.accounts = require("./models/account")(sequelize, Sequelize);
+const db = {
+  Sequelize: Sequelize,
+  sequelize: sequelize,
+  Account: Account,
+};
 
 module.exports = db;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 const { __EXPRESS_PORT__ } = require("./utilities/constants");
 const { login, loginCallback } = require("./controllers/login-controller");
 
-const db = require("./database");
-db.sequelize.sync({ force: true });
+const main = async () => {
+  const db = require("./database");
+  await db.sequelize.sync({ force: true });
 
-const express = require("express");
+  const fuck = db.Account.build({ github_id: 1});
+  await fuck.save();
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+  const express = require("express");
 
-/******************************************************************************/
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
 
-app.get("/login", login);
-app.get("/login/callback", loginCallback);
+  /******************************************************************************/
 
-app.listen(__EXPRESS_PORT__, () => {
-  console.log(`running on port ${__EXPRESS_PORT__}`);
+  app.get("/login", login);
+  app.get("/login/callback", loginCallback);
+
+  app.listen(__EXPRESS_PORT__, () => {
+    console.log(`running on port ${__EXPRESS_PORT__}`);
+  });
+};
+
+main().catch((error) => {
+  console.log(error);
 });
