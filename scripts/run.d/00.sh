c9ca6fa3783a80f52d6cab8759bd58069ddb4a7f
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-26 02:29:18 -0700

    curl scripts

---
+++ scripts/run.d/00.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "==============================================================================="
+curl --location --request GET 'http://localhost:3001/posts' \
+    --header 'x-access-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiZW1haWwiOiJrZXZpbmphbWVzQGdtYWlsLmNvbSIsImlhdCI6MTYyOTY5NzY5OX0.hnVZJpm6SflYnuFmXURtG6WbAT081kyZhIb7SyxJ0Po' \
+    --header 'Authorization: Bearer test1' 2>/dev/null | jq '.'
+
+echo "==============================================================================="
+curl --location --request GET 'http://localhost:3001/posts' \
+    --header 'x-access-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiZW1haWwiOiJrZXZpbmphbWVzQGdtYWlsLmNvbSIsImlhdCI6MTYyOTY5NzY5OX0.hnVZJpm6SflYnuFmXURtG6WbAT081kyZhIb7SyxJ0Po' \
+    --header 'Authorization: Bearer test2' 2>/dev/null | jq '.'
+
+echo "==============================================================================="
+curl --location --request GET 'http://localhost:3001/posts' \
+    --header 'x-access-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiZW1haWwiOiJrZXZpbmphbWVzQGdtYWlsLmNvbSIsImlhdCI6MTYyOTY5NzY5OX0.hnVZJpm6SflYnuFmXURtG6WbAT081kyZhIb7SyxJ0Po' \
+    --header 'Authorization: Bearer test3' 2>/dev/null | jq '.'
