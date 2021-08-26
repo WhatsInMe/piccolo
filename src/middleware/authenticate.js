dafe40f8f344324b2fe9d6ab8243a348f8ca8f13
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-25 21:57:56 -0700

    check header

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,7 +1,13 @@
 const authenticate = (req, res, next) => {
-  const accessToken = req.headers["authorization"].split(" ")[1];
-  console.log(accessToken);
-  next();
+  const accessToken = req.headers["authorization"]
+    ? req.headers["authorization"].split(" ")[1]
+    : undefined;
+  if (!accessToken) {
+    res.sendStatus(403);
+  } else {
+    console.log(accessToken);
+    next();
+  }
 };
 
 module.exports = authenticate;
