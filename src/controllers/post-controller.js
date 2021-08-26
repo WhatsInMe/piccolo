7b6e9125d5000d2d9f42bf362b8057fb4e502ef9
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-25 22:56:00 -0700

    get posts for authorized user

--- src/controllers/post-controller.js
+++ src/controllers/post-controller.js
@@ -1,5 +1,15 @@
-const getPosts = (req, res) => {
-  res.json({ mes: "controller" });
+const db = require("../database");
+
+const getPosts = async (req, res) => {
+  const accessToken = req.headers["authorization"].split(" ")[1];
+  await db.Account.findAll({
+    where: {
+      access_token: accessToken,
+    },
+  }).then(([account]) => {
+    console.log(account.github_id);
+  });
+  res.json({ mes: accessToken });
 };
 
 module.exports = { getPosts };
