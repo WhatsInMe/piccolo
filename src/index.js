32fade2b4270adf4dcaff6702ff9ddeb91026b91
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-28 01:49:44 -0700

    debugging

--- src/index.js
+++ src/index.js
@@ -19,27 +19,53 @@ process.on("SIGINT", () => {
 });
 
 const main = async () => {
+  //      _       _        _
+  //   __| | __ _| |_ __ _| |__   __ _ ___  ___
+  //  / _` |/ _` | __/ _` | '_ \ / _` / __|/ _ \
+  // | (_| | (_| | || (_| | |_) | (_| \__ \  __/
+  //  \__,_|\__,_|\__\__,_|_.__/ \__,_|___/\___|
+
   await db.sequelize.sync({ force: true }).then(async () => {
     console.log("migration complete");
     await seed();
   });
 
+  /**
+   * debug
+   */
   await db.Item.findOne({
     where: {
       id: 1,
     },
   }).then((item) => {
+    console.log(
+      "================================================================================"
+    );
     console.log(JSON.stringify(item));
   });
+
+  await db.Account.findOne({
+    where: {
+      id: 1,
+    },
+  }).then((account) => {
+    console.log(
+      "================================================================================"
+    );
+    console.log(JSON.stringify(account));
+  });
+
+  //                  _
+  //  _ __ ___  _   _| |_ ___  ___
+  // | '__/ _ \| | | | __/ _ \/ __|
+  // | | | (_) | |_| | ||  __/\__ \
+  // |_|  \___/ \__,_|\__\___||___/
 
   const app = express();
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
-  /**
-   * routes
-   */
   app.get("/login", login);
   app.get("/login/callback", loginCallback);
   app.get("/login/status", authenticate, loginStatus);
